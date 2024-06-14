879e9574078e92a71aadd8847c0d8703f66f9821
Ashima Nair <ashima.nair01@gmail.com>
2024-06-14 11:13:41 -0500

    Initial Commit

---
+++ Navigation/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Scholar on 6/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                NavigationLink(destination: SecondView()) {
+                    Text("Take me to a new view")
+                        .foregroundColor(.purple)
+                }
+                Text("This is the root view 🌳")
+                
+            }
+            .toolbar {
+                ToolbarItemGroup(placement : .status){
+                    NavigationLink(destination: SecondView()) {
+                        Text("Second View")
+                    }
+                }
+                /*.navigationTitle("Home")
+                 .navigationBarTitleDisplayMode(.inline)
+                 .navigationBarBackButtonHidden()*/
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  Navigation
+//
+//  Created by Scholar on 6/7/24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Welcome to the Second View!")
+    }
+}
+
+#Preview {
+    SecondView()
+}
